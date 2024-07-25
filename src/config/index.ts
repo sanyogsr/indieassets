ae2aebbe40a102c5373c6b3bc31ded59a8492500
Sanyog Singh Rajput <sanyogsingrajput@gmail.com>
2024-07-25 23:14:09 +0530

    made landing page

---
+++ src/config/index.ts
@@ -0,0 +1,44 @@
+export const PRODUCT_CATEGORIES = [
+    {
+        label: "Ui Kits",
+        value: "ui_kits" as const,
+        featured:[
+            {
+                name: "Editor picks",
+                href:"#",
+                imageSrc:"/nav/ui-kits/mixed.jpg"
+            },
+            {
+                name: "Editor picks",
+                href:"#",
+                imageSrc:"/nav/ui-kits/blue.jpg"
+            },
+            {
+                name: "Editor picks",
+                href:"#",
+                imageSrc:"/nav/ui-kits/purple.jpg"
+            }
+        ]
+    },
+    {
+        label: "Icons",
+        value: "icons" as const,
+        featured:[
+            {
+                name: "Bestselling",
+                href:"#",
+                imageSrc:"/nav/icons/bestseller.jpg"
+            },
+            {
+                name: "New Arrivals",
+                href:"#",
+                imageSrc:"/nav/icons/new.jpg"
+            },
+            {
+                name: "Favourite Icon picks",
+                href:"#",
+                imageSrc:"/nav/icons/picks.jpg"
+            }
+        ]
+    }
+]
